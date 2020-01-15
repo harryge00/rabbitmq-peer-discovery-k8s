73c38d51c503b8f25971965a7c682198849ef5b9
harry ge <harryge@qq.com>
2020-01-16 00:11:25 +0800

    Add debug log.

--- src/rabbit_peer_discovery_k8s.erl
+++ src/rabbit_peer_discovery_k8s.erl
@@ -35,7 +35,7 @@
 %%
 
 init() ->
-    rabbit_log:debug("Peer discovery Kubernetes: initialising..."),
+    rabbit_log:debug("Peer discovery Kubernetes: debug initialising..."),
     ok = application:ensure_started(inets),
     %% we cannot start this plugin yet since it depends on the rabbit app,
     %% which is in the process of being started by the time this function is called
@@ -49,6 +49,7 @@ list_nodes() ->
     case make_request() of
 	{ok, Response} ->
 	    Addresses = extract_node_list(Response),
+      rabbit_log.debug("~s", [Response])
 	    {ok, {lists:map(fun node_name/1, Addresses), disc}};
 	{error, Reason} ->
 	    Details = io_lib:format("Failed to fetch a list of nodes from Kubernetes API: ~s", [Reason]),
